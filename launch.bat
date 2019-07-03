4bada2a6eec7ef3e9eaf6a820aab1f7bf57bdded
Kieran Farr <kieran.farr@gmail.com>
2019-07-03 00:19:55 -0700

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,12 +1,12 @@
 :: This is a convenience launcher for Windows assuming:
-:: (1) SteamVR is installed and configured properly via Steam
+:: (1) SteamVR is installed and you've run room setup with the border of your play area
 :: (2) You've already run npm install on project root directory and server/ directory
 :: (3) You've already compiled a compatible version of exokit in ../exokit/
 :: (4) You understand how to configure the rest of this project.
 ::
 :: Make it fancy by creating a shortcut on your desktop with a pretty icon linking to this batch file.
 
-:: Open code editor, default is Atom
+:: Open code editor, I use Atom
 start /b atom .
 
 :: Open SteamVR
@@ -19,9 +19,11 @@ start npm start
 cd server
 start npm start
 
-:: Start Browser after delay because web host isn't ready yet
-cd ..\..\exokit
+:: Wait 10 seconds while servers start before launching browser
 ping -n 10 127.0.0.1
+
+:: Start Exokit browser
+cd ..\..\exokit
 node . -x webvr http://localhost:3000
 
 exit
